0bc960eee726ba79cd3a46e5b03a101df2090a5b
Arsenii White <arseniizaripov@yandex.ru>
2024-06-03 13:12:23 +0500

    0.2

--- components/CustomTexts.jsx
+++ components/CustomTexts.jsx
@@ -1,13 +1,12 @@
 'use client';
 
-import { motion } from "framer-motion";
-import { textContainer, textVariant2 } from "../utils/motion";
+import { motion } from 'framer-motion';
+import { textContainer, textVariant2 } from '../utils/motion';
 
 export const TypingText = ({ title, textStyles }) => (
   <motion.p
     variants={textContainer}
-    className={`font-normal text-[14px] text-secondary-white
-    ${textStyles}`}
+    className={`font-normal text-[14px] text-secondary-white ${textStyles}`}
   >
     {Array.from(title).map((letter, index) => (
       <motion.span
@@ -25,8 +24,7 @@ export const TitleText = ({ title, textStyles }) => (
     variants={textVariant2}
     initial="hidden"
     whileInView="show"
-    className={`mt-[8px] font-bold md:text-[64px]
-    text-[40px] text-white ${textStyles}`}
+    className={`mt-[8px] font-bold md:text-[64px] text-[40px] text-white ${textStyles}`}
   >
     {title}
   </motion.h2>
